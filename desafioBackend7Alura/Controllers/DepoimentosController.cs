f397f5a56046bd812e797aa12022df78cb6a4e8f
Mateus Rodrigues Silva <m.rodriguessilva75@outlook.com>
2023-11-05 14:26:22 -0300

    Ajustando namespace para escopo de arquivo

--- desafioBackend7Alura/Controllers/DepoimentosController.cs
+++ desafioBackend7Alura/Controllers/DepoimentosController.cs
@@ -4,85 +4,84 @@ using desafioBackend7Alura.Requests.Depoimentos;
 using desafioBackend7Alura.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace desafioBackend7Alura.Controllers
+namespace desafioBackend7Alura.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DepoimentosController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class DepoimentosController : ControllerBase
+    private readonly ILogger<DepoimentosController> _logger;
+    private readonly IDepoimentoService _depoimentoService;
+
+    public DepoimentosController(ILogger<DepoimentosController> logger, IDepoimentoService depoimentoService)
     {
-        private readonly ILogger<DepoimentosController> _logger;
-        private readonly IDepoimentoService _depoimentoService;
+        _logger = logger;
+        _depoimentoService = depoimentoService;
+    }
 
-        public DepoimentosController(ILogger<DepoimentosController> logger, IDepoimentoService depoimentoService)
+    [HttpPost]
+    public IActionResult Criar([FromForm]CriarDepoimentoRequest obj)
+    {
+        try
         {
-            _logger = logger;
-            _depoimentoService = depoimentoService;
-        }
-
-        [HttpPost]
-        public IActionResult Criar([FromForm]CriarDepoimentoRequest obj)
-        {
-            try
-            {
-                var depoimento = _depoimentoService.Criar(obj);
+            var depoimento = _depoimentoService.Criar(obj);
                 
-                return Created($"{nameof(BuscarPorId)}/{depoimento.Id}", depoimento.Id);
-            }
-            catch (ValidationException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (ArgumentNullException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex.StackTrace);
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
+            return Created($"{nameof(BuscarPorId)}/{depoimento.Id}", depoimento.Id);
         }
-
-        [HttpGet("{id:guid}")]
-        public IActionResult BuscarPorId(Guid id)
+        catch (ValidationException ex)
         {
-            try
-            {
-                throw new NotImplementedException();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex.StackTrace);
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
+            return BadRequest(ex.Message);
         }
-
-        [HttpPut]
-        public IActionResult Atualizar()
+        catch (ArgumentNullException ex)
         {
-            try
-            {
-                throw new NotImplementedException();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex.StackTrace);
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
+            return BadRequest(ex.Message);
         }
-
-        [HttpDelete]
-        public IActionResult Excluir()
+        catch (Exception ex)
         {
-            try
-            {
-                throw new NotImplementedException();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message, ex.StackTrace);
-                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
-            }
+            _logger.LogError(ex.Message, ex.StackTrace);
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult BuscarPorId(Guid id)
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex.StackTrace);
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public IActionResult Atualizar()
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex.StackTrace);
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete]
+    public IActionResult Excluir()
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, ex.StackTrace);
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
         }
     }
 }
